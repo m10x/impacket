3910c45935db07e2566e0c9460d1943c2e2e69e0
Maximilian Hildebrand <github@m10x.de>
2026-09-03 12:59:20 +0200

    Revert "New --shadow-credentials and --interactive option to backup (and clea…"
    This reverts commit ce5e948ad87df2226917048f07dc9a0ecf7cf037.

--- impacket/examples/ntlmrelayx/utils/config.py
+++ impacket/examples/ntlmrelayx/utils/config.py
@@ -111,8 +111,6 @@ class NTLMRelayxConfig:
         self.ShadowCredentialsPFXPassword = None
         self.ShadowCredentialsExportType = None
         self.ShadowCredentialsOutfilePath = None
-        self.ShadowCredentialsReplace = False
-        self.ShadowCredentialsBackupPath = None
 
         # SCCM attacks options
         self.isSCCMPoliciesAttack = False
@@ -275,15 +273,11 @@ class NTLMRelayxConfig:
     def setIsShadowCredentialsAttack(self, IsShadowCredentialsAttack):
         self.IsShadowCredentialsAttack = IsShadowCredentialsAttack
 
-    def setShadowCredentialsOptions(self, ShadowCredentialsTarget, ShadowCredentialsPFXPassword, ShadowCredentialsExportType,
-                                    ShadowCredentialsOutfilePath, ShadowCredentialsReplace=False,
-                                    ShadowCredentialsBackupPath=None):
+    def setShadowCredentialsOptions(self, ShadowCredentialsTarget, ShadowCredentialsPFXPassword, ShadowCredentialsExportType, ShadowCredentialsOutfilePath):
         self.ShadowCredentialsTarget = ShadowCredentialsTarget
         self.ShadowCredentialsPFXPassword = ShadowCredentialsPFXPassword
         self.ShadowCredentialsExportType = ShadowCredentialsExportType
         self.ShadowCredentialsOutfilePath = ShadowCredentialsOutfilePath
-        self.ShadowCredentialsReplace = ShadowCredentialsReplace
-        self.ShadowCredentialsBackupPath = ShadowCredentialsBackupPath
     
     def setIsSCCMPoliciesAttack(self, isSCCMPoliciesAttack):
         self.isSCCMPoliciesAttack = isSCCMPoliciesAttack
